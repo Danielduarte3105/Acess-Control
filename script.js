add4a0cd7461c2c32b5672fb86b4a933768ee09b
Matheus Freitas <matheusrlcm@gmail.com>
2023-09-27 18:11:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,4 @@
 let container = document.getElementsByClassName('container');
-
-document.getElementById('QrCode').addEventListener('click', function () {
-// URL da página que você deseja abrir em um pop-up
-const url = 'QrCode.html';
-
-const popupWidth = 175;
-const popupHeight = 310;
-
-const left = (window.innerWidth - popupWidth) / 2;
-const top = (window.innerHeight - popupHeight) / 2;
-
-// Configurações para a janela do pop-up
-const windowFeatures = `width=${popupWidth},height=${popupHeight},left=${left},top=${top},resizable=yes,scrollbars=yes`;
-
-// Abre a página em um pop-up
-window.open(url, 'QrCode.html', windowFeatures);
-});
-
-document.getElementById('QrCode1').addEventListener('click', function () {
-// URL da página que você deseja abrir em um pop-up
-const url = 'QrCode.html';
-
-const popupWidth = 175;
-const popupHeight = 310;
-
-const left = (window.innerWidth - popupWidth) / 2;
-const top = (window.innerHeight - popupHeight) / 2;
-
-// Configurações para a janela do pop-up
-const windowFeatures = `width=${popupWidth},height=${popupHeight},left=${left},top=${top},resizable=yes,scrollbars=yes`;
-
-// Abre a página em um pop-up
-window.open(url, 'QrCode.html', windowFeatures);
-});
-
-    document.getElementById('Config').addEventListener('click', function () {
-// URL da página que você deseja abrir em um pop-up
-const url = 'config.html';
-
-const popupWidth = 310;
-const popupHeight = 320;
-
-const left = (window.innerWidth - popupWidth) / 2;
-const top = (window.innerHeight - popupHeight) / 2;
-
-// Configurações para a janela do pop-up
-const windowFeatures = `width=${popupWidth},height=${popupHeight},left=${left},top=${top},resizable=yes,scrollbars=yes`;
-
-// Abre a página em um pop-up
-window.open(url, 'config.html', windowFeatures);
-});
-
-/*document.getElementById('Config1').addEventListener('click', function () {
-// URL da página que você deseja abrir em um pop-up
-const url = 'config.html';
-
-const popupWidth = 310;
-const popupHeight = 320;
-
-const left = (window.innerWidth - popupWidth) / 2;
-const top = (window.innerHeight - popupHeight) / 2;
-
-// Configurações para a janela do pop-up
-const windowFeatures = `width=${popupWidth},height=${popupHeight},left=${left},top=${top},resizable=yes,scrollbars=yes`;
-
-// Abre a página em um pop-up
-window.open(url, 'config.html', windowFeatures);
-});
-*/
-
 let escuro = document.getElementById('Escuro1')
 let darkMode = 0
 const body = document.body
@@ -79,13 +9,35 @@ escuro.addEventListener('click', function () {
         body.style.backgroundColor = "#000000";
         escuro.innerText = "Modo Claro";
         imgCenter.src = "Preto Glitch Rua Logotipo.png"
-        //icon.src = 'sol.png'
         darkMode = 1;
     } else {
         body.style.backgroundColor = "#fff";
         escuro.innerText = "Modo Escuro";
         imgCenter.src = "logo-og.png"
-        //icon.src = 'lua.png'
         darkMode = 0;
     }
 });
+
+// PopUp - Config e QrCode 
+const QrCode1Bt = document.getElementById("QrCode1"); // (Pegando Variaveis da DOM)
+const QrCodeBt = document.getElementById("QrCode");
+const ConfigButton = document.getElementById("Config"); 
+const closePopUp = document.getElementById("closePopUp"); 
+const configFrame = document.getElementById("configFrame"); 
+const configQrCode = document.getElementById("configQrCode"); 
+
+ConfigButton.addEventListener("click", () => { //Função que abre o popUp Configuração
+    configFrame.style.display = "block";
+    closePopUp.style.display = "block";
+});
+
+closePopUp.addEventListener("click", () => { //Função que fechar os popUps
+    configFrame.style.display = "none";
+    closePopUp.style.display = "none";
+    configQrCode.style.display = "none";
+});
+
+function Qrcode() { //Função que abre o popUp do QrCode 
+    configQrCode.style.display = "block";
+    closePopUp.style.display = "block";
+}
