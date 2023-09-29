7607bbebda8bfca58a680753223945718b0da8b3
Matheus Freitas <matheusrlcm@gmail.com>
2023-09-29 20:31:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,20 +24,66 @@ const QrCodeBt = document.getElementById("QrCode");
 const ConfigButton = document.getElementById("Config"); 
 const closePopUp = document.getElementById("closePopUp"); 
 const configFrame = document.getElementById("configFrame"); 
-const configQrCode = document.getElementById("configQrCode"); 
+const configQrCode = document.getElementById("configQrCode");
+let configFramePop = 1;
+let configQrCodePop = 1;
+    
+//Função que fechar o popup se o usuario clicar fora do popup
 
 ConfigButton.addEventListener("click", () => { //Função que abre o popUp Configuração
+    
     configFrame.style.display = "block";
     closePopUp.style.display = "block";
+    //configFramePop = 1;
+    
 });
 
-closePopUp.addEventListener("click", () => { //Função que fechar os popUps
+function FecharPop() { //Função que fechar os popUps
     configFrame.style.display = "none";
     closePopUp.style.display = "none";
     configQrCode.style.display = "none";
-});
+    configFramePop = 0;
+    configQrCodePop = 0;
+};
 
 function Qrcode() { //Função que abre o popUp do QrCode 
     configQrCode.style.display = "block";
     closePopUp.style.display = "block";
+    //configQrCodePop = 1;
 }
+
+function FecharVazio() {
+    if (configQrCodePop == 1 || configFramePop == 1) {
+        FecharPop();
+    }
+    else if (configQrCodePop == 0 || configFramePop == 0) {
+        console.log("Alteração não feita - Fluxo Errado");
+        configFramePop = 1;
+        configQrCodePop = 1;
+    }  
+    else {
+        console.log("Fluxo Encerrado");
+    }
+}
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    function verificarPopUps() {
+        console.log(
+            window.getComputedStyle(configFrame).getPropertyValue('display') === "block",
+            window.getComputedStyle(configQrCode).getPropertyValue('display') === "block"
+        );
+        
+        
+       
+    }
+
+    setInterval(verificarPopUps, 1000);
+});
+
+
+document.body.addEventListener('click', function() {
+    if (configQrCodePop == 1 || configFramePop == 1) {
+    FecharVazio()
+});
+*/
